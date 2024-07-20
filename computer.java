ba79969a60ecdb81b70e7fee7e4eac4276bfa3df
kingsteph23 <philippeaustephen@gmail.com>
2024-07-20 01:06:48 -0400

    Polished up the code
    Added try/catch exception in player class to prevent input errors
    
    TODO:
    decide whether to do splits or not
    
    Polish/test/debug

--- computer.java
+++ computer.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 
 public class computer extends player {
-    // special card values(kings,queens,jacks,aces)
 
     // first two cards
     int compcard1 = deck[new Random().nextInt(deck.length)];
@@ -10,6 +9,7 @@ public class computer extends player {
 
     // first two cards
     void firsttwo() {
+        System.out.println();
         System.out.println("Computer's first card is " + compcard1);
         System.out.println("Computer's second card is hidden");
         System.out.println();
@@ -20,6 +20,7 @@ public class computer extends player {
     void computerturn() {
         // random card in computer dec
         randomnumber = deck[new Random().nextInt(deck.length)];
+        System.out.println();
         System.out.println("Computer's second card is " + compcard2);
         System.out.println("Computer's total after adding the second card: " + comptotal);
 
@@ -41,28 +42,32 @@ public class computer extends player {
 
         // if computer is less than 17, keeps on adding
 
+        System.out.println();
     }
 
     // game potientnal outcomes
     void game() {
-        // if player wins
-        if (total > comptotal && total <= 21 || comptotal > 21) {
-            System.out.println("You have beaten the computer!");
-
-        }
-        // if computer wins
-        if (comptotal > total && comptotal <= 21 || total > 21) {
-            System.out.println("You lost to the computer!");
-
-        }
         // both computer and dealer loses
         if (total > 21 && comptotal > 21) {
             System.out.println("Both you and the computer busted!");
         }
         // if there is a tie
-        if (total == comptotal) {
+        else if (total == comptotal) {
             System.out.println("It's a tie!");
         }
+
+        // if player wins
+        else if ((total > comptotal && total <= 21) || (comptotal > 21)) {
+            System.out.println("You have beaten the computer!");
+
+        }
+        // if computer loses
+        else if ((comptotal > total && comptotal <= 21) || (total > 21)) {
+            System.out.println("You lost to the computer!");
+
+        }
+        // both computer and dealer loses
+
     }
 
 }
--- gameactual.java
+++ gameactual.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
 
 public class gameactual extends computer {
-
+    // variable for user response
     String userresponse;
 
-    void gameactual() {
+    // function
+    void game() {
+        System.out.println();
+        // title
         System.out.println("Welcome to Blackjack!");
+        // loop for game as along as user doesn't decide to exit
         while (userresponse != "exit") {
-
+            // asking for user input
             System.out
                     .println("Would you like to play?(Enter exit to leave the game. If you want to play, enter play)");
             Scanner scan = new Scanner(System.in);
 
             userresponse = scan.nextLine();
-
+            // if user chooses to play
             if (userresponse.equalsIgnoreCase("play")) {
                 computer gameboi = new computer();
                 gameboi.firsttwo();
                 gameboi.userhit();
                 gameboi.computerturn();
                 gameboi.game();
+                // if user chooses to exit
             } else if (userresponse.equalsIgnoreCase("exit")) {
                 System.out.println("Thank you for playing. Have a good day!");
                 break;
+                // if user puts down a invalid response
             } else {
                 System.out.println("Invalid response. Please try again.");
             }
